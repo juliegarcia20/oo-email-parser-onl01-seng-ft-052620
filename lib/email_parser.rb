97df6f7441c93c4146de20211c138a0141a5c3a6
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-09 01:53:27 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,12 +1,16 @@
 class EmailParser
-attr_accessor :emails
 
-def initialize(email)
-  @emails = email
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    # split into an array using the regex of "possible comma followed by white space"
+    # select from the array only those emails that are not blank spaces
+    # select only those unique emails
+    emails.split(/[,?\s]/).select{|email| email != ""}.uniq 
+  end
+
 end
-
-def parse
-  emails.delete(',').split.uniq
-end
-
-end 
